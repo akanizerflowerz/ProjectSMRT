7b018e02619e2992528c2d8aea3a1dec47e98fd6
akanizerflowerz <akanizer.flowerz@gmail.com>
2023-05-09 05:00:54 +0200

    fix bugs

--- php/quizz_system/quizz_system.php
+++ php/quizz_system/quizz_system.php
@@ -21,6 +21,7 @@ include_once '../includes/main.php';
 </style>
 
 <?php
+session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // Récupération de la valeur du bouton radio sélectionné
   if (isset($_POST["reponse_question"])) {
@@ -33,12 +34,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Vous gagné : " . $ligne["point_repanse"];
         echo '<a href="../quiz.php?id='.$ligne["id_quizz"].'"><button class="button">Valider</button></a>';
         echo '</div>';
-        session_start();
+        
         if (isset($_SESSION['user'])) {
           $user_id = $_SESSION['user'];
           $rq = $conn->query("SELECT DISTINCT niveau FROM utilisateur WHERE id_utilisateur='$user_id'");
           $line  = mysqli_fetch_assoc($rq); 
-          $point = $line["niveau"] + 20;
+          $point = $line["niveau"] + $ligne["point_repanse"];
           $rq_add = $conn->prepare("UPDATE utilisateur SET niveau='$point' WHERE id_utilisateur='$user_id'");
           $rq_add->execute();
         } 

3082e9e8f8937ea9de6e6246e904ff2abe21e3a6
akanizerflowerz <akanizer.flowerz@gmail.com>
2023-05-09 16:23:59 +0200

    oui

--- php/quizz_system/quizz_system.php
+++ php/quizz_system/quizz_system.php
@@ -23,25 +23,29 @@ include_once '../includes/main.php';
 <?php
 session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Récupération de la valeur du bouton radio sélectionné
   if (isset($_POST["reponse_question"])) {
     $choix = $_POST["reponse_question"];
     require_once '../bd.php';
-    $req_point = $conn->query('SELECT DISTINCT id_repanse, point_repanse, id_quizz FROM repanse NATURAL JOIN question WHERE id_repanse='. $choix );
+    $req_point = $conn->query('SELECT id_repanse, point_repanse, id_quizz, id_question FROM repanse NATURAL JOIN question WHERE id_repanse='. $choix );
     echo "<div id='main-box'>";
     while($ligne = mysqli_fetch_assoc($req_point)){
         echo "<div id='child'>";
         echo "Vous gagné : " . $ligne["point_repanse"];
         echo '<a href="../quiz.php?id='.$ligne["id_quizz"].'"><button class="button">Valider</button></a>';
         echo '</div>';
-        
         if (isset($_SESSION['user'])) {
           $user_id = $_SESSION['user'];
-          $rq = $conn->query("SELECT DISTINCT niveau FROM utilisateur WHERE id_utilisateur='$user_id'");
-          $line  = mysqli_fetch_assoc($rq); 
-          $point = $line["niveau"] + $ligne["point_repanse"];
-          $rq_add = $conn->prepare("UPDATE utilisateur SET niveau='$point' WHERE id_utilisateur='$user_id'");
-          $rq_add->execute();
+          if (!isset($_SESSION['points_gagnes'])) {
+            $_SESSION['points_gagnes'] = array();
+          }
+          if (!in_array($ligne["id_question"] , $_SESSION['points_gagnes']) and $ligne["point_repanse"] ) {
+            $rq = $conn->query("SELECT DISTINCT niveau FROM utilisateur WHERE id_utilisateur='$user_id'");
+            $line  = mysqli_fetch_assoc($rq); 
+            $point = $line["niveau"] + $ligne["point_repanse"];
+            $rq_add = $conn->prepare("UPDATE utilisateur SET niveau='$point' WHERE id_utilisateur='$user_id'");
+            $rq_add->execute();
+            $_SESSION['points_gagnes'][] = $ligne["id_question"];
+          }
         } 
     }
     
